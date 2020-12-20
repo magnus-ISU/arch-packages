c06499629feacf873319959f0ca293a090e21554
magnus <magnus@iastate.edu>
2020-12-20 03:32:58 -0600

    Removed config files to get me to add them properly. Added keypress for browser

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -75,6 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[] = { "waterfox-g3", 0 };
 
 #include "shiftview.c"
 static Key keys[] = {
@@ -100,6 +101,7 @@ static Key keys[] = {
 //spawning windows
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
 
 	{ MODKEY,                       XK_x,      killclient,     {0} },
 
